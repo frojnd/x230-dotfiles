67892fdb6993f9c0608c16e56bf8aa3a3255eb9f
frojnd <frojnd@frojnd.com>
2018-01-20 21:39:23 +0100

    Small changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ set __fish_git_prompt_char_stagedstate '→ '
 set __fish_git_prompt_char_stashstate '↩ '
 set __fish_git_prompt_char_upstream_ahead '↑ '
 set __fish_git_prompt_char_upstream_behind '↓ '
- 
+
 function fish_prompt
         set last_status $status
         set_color $fish_color_cwd
